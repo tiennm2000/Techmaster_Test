f9b181b1c9afd57a9107dada35f781ecd668d8ef
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2025-01-01 00:59:56 +0700

    bài 3

---
+++ 03/index.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+
+fs.readFile("timestamp.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  //phân tích chuỗi JSON thành object
+  data = JSON.parse(data);
+  const result = data.timestamp
+    .map((item) => {
+      return JSON.stringify(item);
+    })
+    .join(",\n");
+  fs.writeFile("timestamp.txt", result, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Write file success");
+  });
+});
